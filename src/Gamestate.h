06b6599c03a2da0a93ea2b726274d4fff4b96483
ckwok07 <christiankkwok910@gmail.com>
2026-05-08 23:25:44 +0800

    added fields to gamestate

--- src/Gamestate.h
+++ src/Gamestate.h
@@ -5,11 +5,23 @@ using namespace std;
 
 struct GameState {
     // gold shop, board, bench, augments, time, level, traits, items, 
+
+    // basics
     int gold = 0;
     int level = 1;
+    int xp = 0;
     float time = 0.0f;
+    int stage = 0;
 
-    vector<Champion> shop = vector<Champion>(5);
+    // board
     vector<Champion> bench = vector<Champion>(9);
     vector<vector<Champion>> board = vector<vector<Champion>>(4, vector<Champion>(7));
+    int boardUnitCount = 0;
+    vector<int> activeTraits;
+    vector<int> items;
+
+    // shop
+    vector<Champion> shop = vector<Champion>(5);
+    bool shoplocked = false;
+    vector<int> pool;
 };
